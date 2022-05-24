485d27fb69047449a51b5ddc069f82ea30f27d59
Ara Kanic <ji.arakani@gmail.com>
2022-05-24 08:40:32 -0400

    Update reorder.js

--- reorder.js
+++ reorder.js
@@ -15,9 +15,20 @@ function order(words) {
     return newStr.join(" ")
   }
 
+/*
+// Ideal:
+function order(words) {
+  return words
+          .split(' ')
+          .sort((a, b) => a.match(/\d/) - b.match(/\d/))
+          .join(' ')
+}
+*/
+
 order("is2 Thi1s T4est 3a")
 order("4of Fo1r pe6ople g3ood th5e the2")
 order("")
+
 // "is2 Thi1s T4est 3a"  -->  "Thi1s is2 3a T4est"
 // "4of Fo1r pe6ople g3ood th5e the2"  -->  "Fo1r the2 g3ood 4of th5e pe6ople"
 // ""  -->  ""
