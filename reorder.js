65fa8b526db8027e59c6b12e3f99a6a3104fc42b
jiara <ji.arakani@gmail.com>
2022-05-24 08:28:57 -0400

    first commit

---
+++ reorder.js
@@ -0,0 +1,19 @@
+// This function sorts a given string, where each word contains a single number indicating the position the word should have in the result. The input string should only contain valid consecutive numbers.
+function order(words) {
+    let indices = {}
+    let newStr = []
+    words.split(" ").forEach(word => {
+      word.split("").forEach(char => {
+        if (!isNaN(Number(char))) {
+          indices[Number(char) - 1] = word
+        }
+      })
+    })
+    for (let i in indices) {
+      newStr.push(indices[i])
+    }
+    return newStr.join(" ")
+  }
+// "is2 Thi1s T4est 3a"  -->  "Thi1s is2 3a T4est"
+// "4of Fo1r pe6ople g3ood th5e the2"  -->  "Fo1r the2 g3ood 4of th5e pe6ople"
+// ""  -->  ""
